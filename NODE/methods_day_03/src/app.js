94fd66ebad278c18f1e3e1c3ca2e917ac325d5d1
Subrat3066 <{your_github_email}>
2026-03-17 01:15:37 +0530

    added delete and patch

--- NODE/methods_day_03/src/app.js
+++ NODE/methods_day_03/src/app.js
@@ -5,7 +5,6 @@ app.use(express.json())
 
 let notes = []
 
-
 app.post("/notes" ,(req,res)=>{
     notes.push(req.body)
     console.log("Data received")
@@ -14,12 +13,34 @@ app.post("/notes" ,(req,res)=>{
         message:"Data received"
     })
 })
+
 app.get("/notes" , (req ,res)=>{
     console.log("Data provided")
     res.status(200).json({
         message:"data fetched",
         notes:notes
     })
+})
+app.delete("/notes/:index",(req,res)=>{
+    const index = req.params.index
+    
+    delete notes[index]
+
+    res.status(200).json({
+        message:"note deleted successfully"
+    })
+})
+
+app.patch("/notes/:index" , (req,res)=>{
+    const index = req.params.index
+
+    const time = req.body.time
+    notes[index].time = time
+
+    res.status(200).json({
+        messgae:"Upadeted successfully"
+    })
+
 })
 
 module.exports = app
